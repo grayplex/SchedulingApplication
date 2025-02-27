0171f761b78d81f804a9b6e928db8c60931a4393
grayplex <admin@grayplex.com>
2025-02-27 14:45:13 -0600

    Enhance MainForm layout and interactivity
    Updated the InitializeComponent method in MainForm.Designer.cs to create and configure new UI components, including a header, navigation panel, content area, and footer. Increased the form's client size to 1184x725 pixels and changed the title to "Scheduling Application". Added event handlers for button clicks to improve application interactivity.

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,13 +28,309 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pnlHeader = new System.Windows.Forms.Panel();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.pnlNavigation = new System.Windows.Forms.Panel();
+            this.pnlUpcomingAppointments = new System.Windows.Forms.Panel();
+            this.lstUpcomingAppointments = new System.Windows.Forms.ListView();
+            this.colTitle = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colDateTime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lblUpcomingAppointments = new System.Windows.Forms.Label();
+            this.btnReports = new System.Windows.Forms.Button();
+            this.btnCustomers = new System.Windows.Forms.Button();
+            this.btnAppointments = new System.Windows.Forms.Button();
+            this.btnCalendar = new System.Windows.Forms.Button();
+            this.pnlContent = new System.Windows.Forms.Panel();
+            this.pnlFooter = new System.Windows.Forms.Panel();
+            this.lblTimeZone = new System.Windows.Forms.Label();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.lblCopyright = new System.Windows.Forms.Label();
+            this.pnlHeader.SuspendLayout();
+            this.pnlNavigation.SuspendLayout();
+            this.pnlUpcomingAppointments.SuspendLayout();
+            this.pnlFooter.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(212)))));
+            this.pnlHeader.Controls.Add(this.btnLogout);
+            this.pnlHeader.Controls.Add(this.lblWelcome);
+            this.pnlHeader.Controls.Add(this.lblTitle);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(1184, 64);
+            this.pnlHeader.TabIndex = 0;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLogout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(94)))), ((int)(((byte)(184)))));
+            this.btnLogout.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnLogout.FlatAppearance.BorderSize = 2;
+            this.btnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogout.ForeColor = System.Drawing.Color.White;
+            this.btnLogout.Location = new System.Drawing.Point(1075, 17);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(97, 32);
+            this.btnLogout.TabIndex = 2;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.UseVisualStyleBackColor = false;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.ForeColor = System.Drawing.Color.White;
+            this.lblWelcome.Location = new System.Drawing.Point(927, 25);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(142, 17);
+            this.lblWelcome.TabIndex = 1;
+            this.lblWelcome.Text = "Welcome, Username";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(24, 21);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(222, 24);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Scheduling Application";
+            // 
+            // pnlNavigation
+            // 
+            this.pnlNavigation.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.pnlNavigation.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlNavigation.Controls.Add(this.pnlUpcomingAppointments);
+            this.pnlNavigation.Controls.Add(this.btnReports);
+            this.pnlNavigation.Controls.Add(this.btnCustomers);
+            this.pnlNavigation.Controls.Add(this.btnAppointments);
+            this.pnlNavigation.Controls.Add(this.btnCalendar);
+            this.pnlNavigation.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlNavigation.Location = new System.Drawing.Point(0, 64);
+            this.pnlNavigation.Name = "pnlNavigation";
+            this.pnlNavigation.Size = new System.Drawing.Size(240, 661);
+            this.pnlNavigation.TabIndex = 1;
+            // 
+            // pnlUpcomingAppointments
+            // 
+            this.pnlUpcomingAppointments.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlUpcomingAppointments.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(249)))), ((int)(((byte)(255)))));
+            this.pnlUpcomingAppointments.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlUpcomingAppointments.Controls.Add(this.lstUpcomingAppointments);
+            this.pnlUpcomingAppointments.Controls.Add(this.lblUpcomingAppointments);
+            this.pnlUpcomingAppointments.Location = new System.Drawing.Point(10, 198);
+            this.pnlUpcomingAppointments.Name = "pnlUpcomingAppointments";
+            this.pnlUpcomingAppointments.Size = new System.Drawing.Size(217, 374);
+            this.pnlUpcomingAppointments.TabIndex = 4;
+            // 
+            // lstUpcomingAppointments
+            // 
+            this.lstUpcomingAppointments.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstUpcomingAppointments.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colTitle,
+            this.colDateTime});
+            this.lstUpcomingAppointments.HideSelection = false;
+            this.lstUpcomingAppointments.Location = new System.Drawing.Point(7, 37);
+            this.lstUpcomingAppointments.Name = "lstUpcomingAppointments";
+            this.lstUpcomingAppointments.Size = new System.Drawing.Size(200, 327);
+            this.lstUpcomingAppointments.TabIndex = 1;
+            this.lstUpcomingAppointments.UseCompatibleStateImageBehavior = false;
+            this.lstUpcomingAppointments.View = System.Windows.Forms.View.Details;
+            // 
+            // colTitle
+            // 
+            this.colTitle.Text = "Title";
+            this.colTitle.Width = 100;
+            // 
+            // colDateTime
+            // 
+            this.colDateTime.Text = "Date/Time";
+            this.colDateTime.Width = 96;
+            // 
+            // lblUpcomingAppointments
+            // 
+            this.lblUpcomingAppointments.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblUpcomingAppointments.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(212)))));
+            this.lblUpcomingAppointments.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUpcomingAppointments.ForeColor = System.Drawing.Color.White;
+            this.lblUpcomingAppointments.Location = new System.Drawing.Point(0, 0);
+            this.lblUpcomingAppointments.Name = "lblUpcomingAppointments";
+            this.lblUpcomingAppointments.Padding = new System.Windows.Forms.Padding(5);
+            this.lblUpcomingAppointments.Size = new System.Drawing.Size(215, 30);
+            this.lblUpcomingAppointments.TabIndex = 0;
+            this.lblUpcomingAppointments.Text = "Upcoming Appointments";
+            // 
+            // btnReports
+            // 
+            this.btnReports.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnReports.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReports.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReports.Location = new System.Drawing.Point(10, 153);
+            this.btnReports.Name = "btnReports";
+            this.btnReports.Size = new System.Drawing.Size(217, 40);
+            this.btnReports.TabIndex = 3;
+            this.btnReports.Text = "Reports";
+            this.btnReports.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnReports.UseVisualStyleBackColor = true;
+            this.btnReports.Click += new System.EventHandler(this.btnReports_Click);
+            // 
+            // btnCustomers
+            // 
+            this.btnCustomers.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCustomers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCustomers.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCustomers.Location = new System.Drawing.Point(10, 107);
+            this.btnCustomers.Name = "btnCustomers";
+            this.btnCustomers.Size = new System.Drawing.Size(217, 40);
+            this.btnCustomers.TabIndex = 2;
+            this.btnCustomers.Text = "Customers";
+            this.btnCustomers.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCustomers.UseVisualStyleBackColor = true;
+            this.btnCustomers.Click += new System.EventHandler(this.btnCustomers_Click);
+            // 
+            // btnAppointments
+            // 
+            this.btnAppointments.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAppointments.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAppointments.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAppointments.Location = new System.Drawing.Point(10, 61);
+            this.btnAppointments.Name = "btnAppointments";
+            this.btnAppointments.Size = new System.Drawing.Size(217, 40);
+            this.btnAppointments.TabIndex = 1;
+            this.btnAppointments.Text = "Appointments";
+            this.btnAppointments.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAppointments.UseVisualStyleBackColor = true;
+            this.btnAppointments.Click += new System.EventHandler(this.btnAppointments_Click);
+            // 
+            // btnCalendar
+            // 
+            this.btnCalendar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCalendar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalendar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalendar.Location = new System.Drawing.Point(10, 15);
+            this.btnCalendar.Name = "btnCalendar";
+            this.btnCalendar.Size = new System.Drawing.Size(217, 40);
+            this.btnCalendar.TabIndex = 0;
+            this.btnCalendar.Text = "Calendar";
+            this.btnCalendar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCalendar.UseVisualStyleBackColor = true;
+            this.btnCalendar.Click += new System.EventHandler(this.btnCalendar_Click);
+            // 
+            // pnlContent
+            // 
+            this.pnlContent.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlContent.BackColor = System.Drawing.Color.White;
+            this.pnlContent.Location = new System.Drawing.Point(240, 64);
+            this.pnlContent.Name = "pnlContent";
+            this.pnlContent.Size = new System.Drawing.Size(944, 625);
+            this.pnlContent.TabIndex = 2;
+            // 
+            // pnlFooter
+            // 
+            this.pnlFooter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(242)))));
+            this.pnlFooter.Controls.Add(this.lblTimeZone);
+            this.pnlFooter.Controls.Add(this.lblUser);
+            this.pnlFooter.Controls.Add(this.lblCopyright);
+            this.pnlFooter.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlFooter.Location = new System.Drawing.Point(240, 689);
+            this.pnlFooter.Name = "pnlFooter";
+            this.pnlFooter.Size = new System.Drawing.Size(944, 36);
+            this.pnlFooter.TabIndex = 3;
+            // 
+            // lblTimeZone
+            // 
+            this.lblTimeZone.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTimeZone.AutoSize = true;
+            this.lblTimeZone.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(112)))), ((int)(((byte)(112)))));
+            this.lblTimeZone.Location = new System.Drawing.Point(689, 12);
+            this.lblTimeZone.Name = "lblTimeZone";
+            this.lblTimeZone.Size = new System.Drawing.Size(253, 13);
+            this.lblTimeZone.TabIndex = 2;
+            this.lblTimeZone.Text = "Your time zone: Pacific Standard Time (Standard Time)";
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(112)))), ((int)(((byte)(112)))));
+            this.lblUser.Location = new System.Drawing.Point(365, 12);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(108, 13);
+            this.lblUser.TabIndex = 1;
+            this.lblUser.Text = "Logged in as: test";
+            // 
+            // lblCopyright
+            // 
+            this.lblCopyright.AutoSize = true;
+            this.lblCopyright.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(112)))), ((int)(((byte)(112)))));
+            this.lblCopyright.Location = new System.Drawing.Point(20, 12);
+            this.lblCopyright.Name = "lblCopyright";
+            this.lblCopyright.Size = new System.Drawing.Size(172, 13);
+            this.lblCopyright.TabIndex = 0;
+            this.lblCopyright.Text = "© 2025 - Scheduling Application";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1184, 725);
+            this.Controls.Add(this.pnlFooter);
+            this.Controls.Add(this.pnlContent);
+            this.Controls.Add(this.pnlNavigation);
+            this.Controls.Add(this.pnlHeader);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Scheduling Application";
+            this.pnlHeader.ResumeLayout(false);
+            this.pnlHeader.PerformLayout();
+            this.pnlNavigation.ResumeLayout(false);
+            this.pnlUpcomingAppointments.ResumeLayout(false);
+            this.pnlFooter.ResumeLayout(false);
+            this.pnlFooter.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlHeader;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Panel pnlNavigation;
+        private System.Windows.Forms.Button btnCalendar;
+        private System.Windows.Forms.Button btnAppointments;
+        private System.Windows.Forms.Button btnCustomers;
+        private System.Windows.Forms.Button btnReports;
+        private System.Windows.Forms.Panel pnlUpcomingAppointments;
+        private System.Windows.Forms.Label lblUpcomingAppointments;
+        private System.Windows.Forms.ListView lstUpcomingAppointments;
+        private System.Windows.Forms.ColumnHeader colTitle;
+        private System.Windows.Forms.ColumnHeader colDateTime;
+        private System.Windows.Forms.Panel pnlContent;
+        private System.Windows.Forms.Panel pnlFooter;
+        private System.Windows.Forms.Label lblCopyright;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Label lblTimeZone;
     }
 }
 
