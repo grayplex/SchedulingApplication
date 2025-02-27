7ae8c265216ecd3bd6230f9f04468d3f1d2c7822
grayplex <admin@grayplex.com>
2025-02-27 15:49:35 -0600

    Add AppointmentEditorForm for managing appointments
    This commit introduces the `AppointmentEditorForm` to the `SchedulingApplication`, enabling users to add or edit appointments. It includes the form's designer code and main logic, which set up the UI components, load reference data, validate user input, and save appointment data to the database. The project file is also updated to include the new form files in the compilation process.

---
+++ AppointmentEditorForm.Designer.cs
@@ -0,0 +1,250 @@
+﻿using System.Windows.Forms;
+
+namespace SchedulingApplication
+{
+    partial class AppointmentEditorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlHeader = new System.Windows.Forms.Panel();
+            this.lblWindowTitle = new System.Windows.Forms.Label();
+            this.pnlContent = new System.Windows.Forms.Panel();
+            this.pnlButtons = new System.Windows.Forms.Panel();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lblValidation = new System.Windows.Forms.Label();
+
+            // Basic Information Group
+            this.grpBasicInfo = new System.Windows.Forms.GroupBox();
+            this.lblCustomer = new System.Windows.Forms.Label();
+            this.cboCustomer = new System.Windows.Forms.ComboBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.lblType = new System.Windows.Forms.Label();
+            this.cboType = new System.Windows.Forms.ComboBox();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+
+            // Contact Information Group
+            this.grpContactInfo = new System.Windows.Forms.GroupBox();
+            this.lblLocation = new System.Windows.Forms.Label();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.lblContact = new System.Windows.Forms.Label();
+            this.txtContact = new System.Windows.Forms.TextBox();
+            this.lblUrl = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+
+            // Schedule Group
+            this.grpSchedule = new System.Windows.Forms.GroupBox();
+            this.lblStartDate = new System.Windows.Forms.Label();
+            this.dtStart = new System.Windows.Forms.DateTimePicker();
+            this.lblEndDate = new System.Windows.Forms.Label();
+            this.dtEnd = new System.Windows.Forms.DateTimePicker();
+
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(212)))));
+            this.pnlHeader.Controls.Add(this.lblWindowTitle);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Height = 60;
+
+            // 
+            // lblWindowTitle
+            // 
+            this.lblWindowTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold);
+            this.lblWindowTitle.ForeColor = System.Drawing.Color.White;
+            this.lblWindowTitle.Location = new System.Drawing.Point(10, 15);
+            this.lblWindowTitle.Text = "Add/Edit Appointment";
+
+            // 
+            // pnlContent
+            // 
+            this.pnlContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlContent.Padding = new System.Windows.Forms.Padding(20);
+
+            // 
+            // pnlButtons
+            // 
+            this.pnlButtons.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlButtons.Height = 60;
+            this.pnlButtons.Controls.AddRange(new System.Windows.Forms.Control[] { this.btnCancel, this.btnSave });
+
+            // 
+            // btnSave
+            // 
+            this.btnSave.Text = "Save";
+            this.btnSave.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnSave.Location = new System.Drawing.Point(400, 15);
+            this.btnSave.DialogResult = System.Windows.Forms.DialogResult.OK;
+
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnCancel.Location = new System.Drawing.Point(510, 15);
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+
+            // 
+            // lblValidation
+            // 
+            this.lblValidation.ForeColor = System.Drawing.Color.Red;
+            this.lblValidation.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblValidation.Height = 30;
+
+            // Basic Information Group
+            // 
+            this.grpBasicInfo.Text = "Basic Information";
+            this.grpBasicInfo.Dock = System.Windows.Forms.DockStyle.Top;
+
+            // Customer Dropdown
+            this.lblCustomer.Text = "Customer:";
+            this.cboCustomer.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+
+            // Title Textbox
+            this.lblTitle.Text = "Title:";
+
+            // Type Dropdown
+            this.lblType.Text = "Type:";
+            this.cboType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+
+            // Description Textbox
+            this.lblDescription.Text = "Description:";
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Height = 80;
+
+            // Contact Information Group
+            this.grpContactInfo.Text = "Contact Information";
+            this.grpContactInfo.Dock = System.Windows.Forms.DockStyle.Top;
+
+            // Location Textbox
+            this.lblLocation.Text = "Location:";
+
+            // Contact Textbox
+            this.lblContact.Text = "Contact:";
+
+            // URL Textbox
+            this.lblUrl.Text = "URL:";
+
+            // Schedule Group
+            this.grpSchedule.Text = "Schedule";
+            this.grpSchedule.Dock = System.Windows.Forms.DockStyle.Top;
+
+            // Start Date/Time Picker
+            this.lblStartDate.Text = "Start Date/Time:";
+            this.dtStart.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtStart.CustomFormat = "MM/dd/yyyy hh:mm tt";
+            this.dtStart.ShowUpDown = true;
+
+            // End Date/Time Picker
+            this.lblEndDate.Text = "End Date/Time:";
+            this.dtEnd.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtEnd.CustomFormat = "MM/dd/yyyy hh:mm tt";
+            this.dtEnd.ShowUpDown = true;
+
+            // Organize Layout
+            this.grpBasicInfo.Controls.AddRange(new System.Windows.Forms.Control[] {
+            this.lblCustomer, this.cboCustomer,
+            this.lblTitle, this.txtTitle,
+            this.lblType, this.cboType,
+            this.lblDescription, this.txtDescription
+        });
+
+            this.grpContactInfo.Controls.AddRange(new System.Windows.Forms.Control[] {
+            this.lblLocation, this.txtLocation,
+            this.lblContact, this.txtContact,
+            this.lblUrl, this.txtUrl
+        });
+
+            this.grpSchedule.Controls.AddRange(new System.Windows.Forms.Control[] {
+            this.lblStartDate, this.dtStart,
+            this.lblEndDate, this.dtEnd
+        });
+
+            this.pnlContent.Controls.AddRange(new System.Windows.Forms.Control[] {
+            this.grpSchedule,
+            this.grpContactInfo,
+            this.grpBasicInfo,
+            this.lblValidation
+        });
+
+            // 
+            // AppointmentEditorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 550);
+            this.Controls.AddRange(new System.Windows.Forms.Control[] {
+            this.pnlContent,
+            this.pnlButtons,
+            this.pnlHeader
+        });
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AppointmentEditorForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Add/Edit Appointment";
+
+            // Perform layout
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlHeader;
+        private System.Windows.Forms.Label lblWindowTitle;
+        private System.Windows.Forms.Panel pnlContent;
+        private System.Windows.Forms.Panel pnlButtons;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblValidation;
+        private System.Windows.Forms.GroupBox grpBasicInfo;
+        private System.Windows.Forms.Label lblCustomer;
+        private System.Windows.Forms.ComboBox cboCustomer;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label lblType;
+        private System.Windows.Forms.ComboBox cboType;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.GroupBox grpContactInfo;
+        private System.Windows.Forms.Label lblLocation;
+        private System.Windows.Forms.TextBox txtLocation;
+        private System.Windows.Forms.Label lblContact;
+        private System.Windows.Forms.TextBox txtContact;
+        private System.Windows.Forms.Label lblUrl;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.GroupBox grpSchedule;
+        private System.Windows.Forms.Label lblStartDate;
+        private System.Windows.Forms.DateTimePicker dtStart;
+        private System.Windows.Forms.Label lblEndDate;
+        private System.Windows.Forms.DateTimePicker dtEnd;
+    }
+}
---
+++ AppointmentEditorForm.cs
@@ -0,0 +1,306 @@
+﻿using SchedulingApplication.Models;
+using SchedulingApplication.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SchedulingApplication
+{
+    public partial class AppointmentEditorForm : Form
+    {
+        private Appointment _appointment;
+        private bool _isNewAppointment;
+        private string _validationMessage;
+        private List<Customer> _customers;
+        private List<string> _appointmentTypes;
+
+        public AppointmentEditorForm(Appointment appointment, bool isNewAppointment)
+        {
+            InitializeComponent();
+
+            // Initialize main properties
+            _appointment = appointment;
+            _isNewAppointment = isNewAppointment;
+
+            // Set window title
+            Text = isNewAppointment ? "Add New Appointment" : "Edit Appointment";
+
+            // Load reference data
+            LoadReferenceData();
+
+            // Populate fields
+            PopulateAppointmentFields();
+
+            // Setup event handlers
+            btnSave.Click += BtnSave_Click;
+            btnCancel.Click += BtnCancel_Click;
+        }
+
+        private void LoadReferenceData()
+        {
+            try
+            {
+                // Load customers
+                _customers = Program.DbContext.Customers
+                    .Where(c => c.Active)
+                    .OrderBy(c => c.CustomerName)
+                    .ToList();
+                cboCustomer.DataSource = _customers;
+                cboCustomer.DisplayMember = "CustomerName";
+                cboCustomer.ValueMember = "CustomerId";
+
+                // Appointment types
+                _appointmentTypes = new List<string>
+                {
+                    "Initial Consultation",
+                    "Follow-up",
+                    "Planning Session",
+                    "De-Briefing",
+                    "Presentation",
+                    "Other"
+                };
+                cboType.DataSource = _appointmentTypes;
+
+                // If editing, set initial selection
+                if (!_isNewAppointment)
+                {
+                    // Set customer
+                    cboCustomer.SelectedValue = _appointment.CustomerId;
+                    cboType.SelectedItem = _appointment.Type;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading reference data: {ex.Message}", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void PopulateAppointmentFields()
+        {
+            // If editing existing appointment
+            if (!_isNewAppointment)
+            {
+                // Convert times to user's local time
+                _appointment.Start = TimeZoneHelper.ToUserTime(_appointment.Start);
+                _appointment.End = TimeZoneHelper.ToUserTime(_appointment.End);
+
+                // Populate fields
+                txtTitle.Text = _appointment.Title;
+                txtDescription.Text = _appointment.Description;
+                txtLocation.Text = _appointment.Location;
+                txtContact.Text = _appointment.Contact;
+                txtUrl.Text = _appointment.Url;
+
+                dtStart.Value = _appointment.Start;
+                dtEnd.Value = _appointment.End;
+            }
+            else
+            {
+                // Default times for new appointments
+                dtStart.Value = DateTime.Today.AddHours(9);  // 9 AM
+                dtEnd.Value = DateTime.Today.AddHours(10);   // 10 AM
+            }
+        }
+
+        private bool ValidateAppointment()
+        {
+            // Clear previous validation
+            lblValidation.Text = string.Empty;
+
+            // Validate required fields
+            if (cboCustomer.SelectedItem == null)
+            {
+                lblValidation.Text = "Customer is required.";
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtTitle.Text))
+            {
+                lblValidation.Text = "Title is required.";
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtDescription.Text))
+            {
+                lblValidation.Text = "Description is required.";
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtLocation.Text))
+            {
+                lblValidation.Text = "Location is required.";
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtContact.Text))
+            {
+                lblValidation.Text = "Contact is required.";
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(cboType.Text))
+            {
+                lblValidation.Text = "Type is required.";
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtUrl.Text))
+            {
+                lblValidation.Text = "URL is required.";
+                return false;
+            }
+
+            // Validate date/time
+            if (dtEnd.Value <= dtStart.Value)
+            {
+                lblValidation.Text = "End time must be after start time.";
+                return false;
+            }
+
+            // Validate business hours (in Eastern Time)
+            var startEST = TimeZoneHelper.ToBusinessHoursTimeZone(dtStart.Value);
+            var endEST = TimeZoneHelper.ToBusinessHoursTimeZone(dtEnd.Value);
+
+            // Check for weekday
+            if (startEST.DayOfWeek == DayOfWeek.Saturday ||
+                startEST.DayOfWeek == DayOfWeek.Sunday ||
+                endEST.DayOfWeek == DayOfWeek.Saturday ||
+                endEST.DayOfWeek == DayOfWeek.Sunday)
+            {
+                lblValidation.Text = "Appointments must be on weekdays.";
+                return false;
+            }
+
+            // Check business hours
+            var businessStart = new TimeSpan(9, 0, 0);
+            var businessEnd = new TimeSpan(17, 0, 0);
+
+            if (startEST.TimeOfDay < businessStart ||
+                startEST.TimeOfDay > businessEnd ||
+                endEST.TimeOfDay < businessStart ||
+                endEST.TimeOfDay > businessEnd)
+            {
+                lblValidation.Text = "Appointments must be between 9 AM and 5 PM Eastern Time.";
+                return false;
+            }
+
+            // Check for overlapping appointments for the same user
+            var overlappingAppointments = Program.DbContext.Appointments
+                .Where(a => a.UserId == LoginForm.LoggedInUser.UserId &&
+                            a.AppointmentId != _appointment.AppointmentId &&
+                            (
+                                (a.Start <= dtStart.Value && a.End > dtStart.Value) ||
+                (a.Start < dtEnd.Value && a.End >= dtEnd.Value) ||
+                                (a.Start >= dtStart.Value && a.End <= dtEnd.Value)
+                            ))
+                .ToList();
+
+            if (overlappingAppointments.Any())
+            {
+                lblValidation.Text = "This appointment overlaps with an existing appointment.";
+                return false;
+            }
+
+            return true;
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Validate appointment
+                if (!ValidateAppointment())
+                {
+                    return;
+                }
+
+                // Convert start and end times from local to UTC
+                var startTime = TimeZoneHelper.ToDatabaseTime(dtStart.Value);
+                var endTime = TimeZoneHelper.ToDatabaseTime(dtEnd.Value);
+
+                // Update appointment properties
+                _appointment.CustomerId = (int)cboCustomer.SelectedValue;
+                _appointment.UserId = LoginForm.LoggedInUser.UserId;
+                _appointment.Title = txtTitle.Text;
+                _appointment.Description = txtDescription.Text;
+                _appointment.Location = txtLocation.Text;
+                _appointment.Contact = txtContact.Text;
+                _appointment.Type = cboType.Text;
+                _appointment.Url = txtUrl.Text;
+                _appointment.Start = startTime;
+                _appointment.End = endTime;
+
+                // Set audit fields
+                if (_isNewAppointment)
+                {
+                    _appointment.CreateDate = DateTime.Now;
+                    _appointment.CreatedBy = LoginForm.LoggedInUser.UserName;
+                }
+
+                _appointment.LastUpdate = DateTime.Now;
+                _appointment.LastUpdateBy = LoginForm.LoggedInUser.UserName;
+
+                // Save to database
+                using (var transaction = Program.DbContext.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        if (_isNewAppointment)
+                        {
+                            Program.DbContext.Appointments.Add(_appointment);
+                        }
+                        else
+                        {
+                            var existingAppointment = Program.DbContext.Appointments.Find(_appointment.AppointmentId);
+                            if (existingAppointment == null)
+                            {
+                                throw new Exception("Appointment not found.");
+                            }
+
+                            // Update all properties
+                            existingAppointment.CustomerId = _appointment.CustomerId;
+                            existingAppointment.UserId = _appointment.UserId;
+                            existingAppointment.Title = _appointment.Title;
+                            existingAppointment.Description = _appointment.Description;
+                            existingAppointment.Location = _appointment.Location;
+                            existingAppointment.Contact = _appointment.Contact;
+                            existingAppointment.Type = _appointment.Type;
+                            existingAppointment.Url = _appointment.Url;
+                            existingAppointment.Start = _appointment.Start;
+                            existingAppointment.End = _appointment.End;
+                            existingAppointment.LastUpdate = _appointment.LastUpdate;
+                            existingAppointment.LastUpdateBy = _appointment.LastUpdateBy;
+                        }
+
+                        Program.DbContext.SaveChanges();
+                        transaction.Commit();
+
+                        DialogResult = DialogResult.OK;
+                        Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        MessageBox.Show($"Error saving appointment: {ex.Message}", "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+    }
+}
