41c10a93d2758c06fcbd07b85fbdab2c63b2c5ab
grayplex <admin@grayplex.com>
2025-02-27 15:49:47 -0600

    Add AppointmentsUserControl for managing appointments
    This commit introduces a new user control named `AppointmentsUserControl` in the `SchedulingApplication`. The control provides functionalities for filtering, adding, editing, viewing details, and deleting appointments. It is implemented in `AppointmentsUserControl.cs` and `AppointmentsUserControl.Designer.cs`, and integrated into the project file `SchedulingApplication.csproj` to ensure proper recognition and compilation.

---
+++ AppointmentsUserControl.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace SchedulingApplication
+{
+    partial class AppointmentsUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlHeader = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+
+            this.pnlFilter = new System.Windows.Forms.Panel();
+            this.lblTimeFilter = new System.Windows.Forms.Label();
+            this.cbTimeFilter = new System.Windows.Forms.ComboBox();
+            this.lblStartDate = new System.Windows.Forms.Label();
+            this.dtStart = new System.Windows.Forms.DateTimePicker();
+            this.lblEndDate = new System.Windows.Forms.Label();
+            this.dtEnd = new System.Windows.Forms.DateTimePicker();
+            this.btnAddAppointment = new System.Windows.Forms.Button();
+
+            this.pnlGrid = new System.Windows.Forms.Panel();
+            this.dgvAppointments = new System.Windows.Forms.DataGridView();
+
+            this.pnlButtons = new System.Windows.Forms.Panel();
+            this.btnViewDetails = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.Controls.Add(this.lblTitle);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Height = 50;
+
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Text = "Appointments";
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+
+            // 
+            // pnlFilter
+            // 
+            this.pnlFilter.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlFilter.Height = 60;
+            this.pnlFilter.BackColor = System.Drawing.Color.White;
+            this.pnlFilter.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+
+            // 
+            // lblTimeFilter
+            // 
+            this.lblTimeFilter.Text = "Filter:";
+            this.lblTimeFilter.Location = new System.Drawing.Point(10, 15);
+            this.lblTimeFilter.Width = 100;
+
+            // 
+            // cbTimeFilter
+            // 
+            this.cbTimeFilter.Location = new System.Drawing.Point(120, 12);
+            this.cbTimeFilter.Width = 200;
+            this.cbTimeFilter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+
+            // 
+            // lblStartDate
+            // 
+            this.lblStartDate.Text = "Start:";
+            this.lblStartDate.Location = new System.Drawing.Point(330, 15);
+            this.lblStartDate.Width = 100;
+
+            // 
+            // dtStart
+            // 
+            this.dtStart.Location = new System.Drawing.Point(430, 12);
+            this.dtStart.Width = 120;
+            this.dtStart.Enabled = false;
+
+            // 
+            // lblEndDate
+            // 
+            this.lblEndDate.Text = "End:";
+            this.lblEndDate.Location = new System.Drawing.Point(560, 15);
+            this.lblEndDate.Width = 100;
+
+            // 
+            // dtEnd
+            // 
+            this.dtEnd.Location = new System.Drawing.Point(660, 12);
+            this.dtEnd.Width = 120;
+            this.dtEnd.Enabled = false;
+
+            // 
+            // btnAddAppointment
+            // 
+            this.btnAddAppointment.Text = "Add Appointment";
+            this.btnAddAppointment.Location = new System.Drawing.Point(790, 12);
+            this.btnAddAppointment.Width = 120;
+            this.btnAddAppointment.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(212)))));
+            this.btnAddAppointment.ForeColor = System.Drawing.Color.White;
+
+            // 
+            // pnlGrid
+            // 
+            this.pnlGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlGrid.Padding = new System.Windows.Forms.Padding(10);
+
+            // 
+            // dgvAppointments
+            // 
+            this.dgvAppointments.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvAppointments.AllowUserToAddRows = false;
+            this.dgvAppointments.AllowUserToDeleteRows = false;
+            this.dgvAppointments.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvAppointments.ReadOnly = true;
+
+            // Configure DataGridView Columns
+            this.dgvAppointments.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+                new System.Windows.Forms.DataGridViewTextBoxColumn {
+                    Name = "Id",
+                    HeaderText = "ID",
+                    Visible = false
+                },
+                new System.Windows.Forms.DataGridViewTextBoxColumn {
+                    Name = "Title",
+                    HeaderText = "Title",
+                    Width = 180
+                },
+                new System.Windows.Forms.DataGridViewTextBoxColumn {
+                    Name = "Customer",
+                    HeaderText = "Customer",
+                    Width = 180
+                },
+                new System.Windows.Forms.DataGridViewTextBoxColumn {
+                    Name = "Type",
+                    HeaderText = "Type",
+                    Width = 120
+                },
+                new System.Windows.Forms.DataGridViewTextBoxColumn {
+                    Name = "Start",
+                    HeaderText = "Start",
+                    Width = 170
+                },
+                new System.Windows.Forms.DataGridViewTextBoxColumn {
+                    Name = "End",
+                    HeaderText = "End",
+                    Width = 170
+                },
+                new System.Windows.Forms.DataGridViewTextBoxColumn {
+                    Name = "Location",
+                    HeaderText = "Location",
+                    Width = 150
+                }
+            });
+
+            // 
+            // pnlButtons
+            // 
+            this.pnlButtons.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlButtons.Height = 60;
+            this.pnlButtons.BackColor = System.Drawing.SystemColors.Control;
+
+            // 
+            // btnViewDetails
+            // 
+            this.btnViewDetails.Text = "View Details";
+            this.btnViewDetails.Enabled = false;
+            this.btnViewDetails.Location = new System.Drawing.Point(400, 15);
+            this.btnViewDetails.Width = 120;
+
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.Enabled = false;
+            this.btnEdit.Location = new System.Drawing.Point(530, 15);
+            this.btnEdit.Width = 120;
+
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.Enabled = false;
+            this.btnDelete.Location = new System.Drawing.Point(660, 15);
+            this.btnDelete.Width = 120;
+
+            // 
+            // AppointmentsUserControl
+            // 
+            this.Controls.Add(this.pnlGrid);
+            this.Controls.Add(this.pnlButtons);
+            this.Controls.Add(this.pnlFilter);
+            this.Controls.Add(this.pnlHeader);
+
+            // Add controls to panels
+            this.pnlGrid.Controls.Add(this.dgvAppointments);
+            this.pnlFilter.Controls.AddRange(new System.Windows.Forms.Control[] {
+                this.lblTimeFilter,
+                this.cbTimeFilter,
+                this.lblStartDate,
+                this.dtStart,
+                this.lblEndDate,
+                this.dtEnd,
+                this.btnAddAppointment
+            });
+            this.pnlButtons.Controls.AddRange(new System.Windows.Forms.Control[] {
+                this.btnViewDetails,
+                this.btnEdit,
+                this.btnDelete
+            });
+
+            this.Name = "AppointmentsUserControl";
+            this.Size = new System.Drawing.Size(900, 600);
+        }
+
+        #endregion
+
+        // Header Panel
+        private System.Windows.Forms.Panel pnlHeader;
+        private System.Windows.Forms.Label lblTitle;
+
+        // Filter Panel
+        private System.Windows.Forms.Panel pnlFilter;
+        private System.Windows.Forms.Label lblTimeFilter;
+        private System.Windows.Forms.ComboBox cbTimeFilter;
+        private System.Windows.Forms.Label lblStartDate;
+        private System.Windows.Forms.DateTimePicker dtStart;
+        private System.Windows.Forms.Label lblEndDate;
+        private System.Windows.Forms.DateTimePicker dtEnd;
+        private System.Windows.Forms.Button btnAddAppointment;
+
+        // Grid Panel
+        private System.Windows.Forms.Panel pnlGrid;
+        private System.Windows.Forms.DataGridView dgvAppointments;
+
+        // Buttons Panel
+        private System.Windows.Forms.Panel pnlButtons;
+        private System.Windows.Forms.Button btnViewDetails;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
---
+++ AppointmentsUserControl.cs
@@ -0,0 +1,253 @@
+﻿using SchedulingApplication.Models;
+using SchedulingApplication.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SchedulingApplication
+{
+    public partial class AppointmentsUserControl : UserControl
+    {
+        private List<Appointment> _appointments;
+        private Appointment _selectedAppointment;
+
+        public AppointmentsUserControl()
+        {
+            InitializeComponent();
+
+            // Setup event handlers
+            cbTimeFilter.SelectedIndexChanged += FilterChanged;
+            dtStart.ValueChanged += FilterChanged;
+            dtEnd.ValueChanged += FilterChanged;
+            btnAddAppointment.Click += BtnAddAppointment_Click;
+            btnViewDetails.Click += BtnViewDetails_Click;
+            btnEdit.Click += BtnEdit_Click;
+            btnDelete.Click += BtnDelete_Click;
+            dgvAppointments.SelectionChanged += DgvAppointments_SelectionChanged;
+
+            // Initialize time filter options
+            cbTimeFilter.Items.AddRange(new string[] {
+                "All Appointments",
+                "This Week",
+                "This Month",
+                "By Date Range"
+            });
+            cbTimeFilter.SelectedIndex = 1; // Default to This Week
+        }
+
+        private void LoadAppointments()
+        {
+            try
+            {
+                var query = Program.DbContext.Appointments
+                    .Include(a => a.Customer)
+                    .Include(a => a.User)
+                    .Where(a => a.UserId == LoginForm.LoggedInUser.UserId);
+
+                // Apply filter based on selected option
+                switch (cbTimeFilter.SelectedItem.ToString())
+                {
+                    case "This Week":
+                        var today = DateTime.Today;
+                        var startOfWeek = today.AddDays(-(int)today.DayOfWeek);
+                        var endOfWeek = startOfWeek.AddDays(7);
+                        query = query.Where(a => a.Start >= startOfWeek && a.Start < endOfWeek);
+                        break;
+
+                    case "This Month":
+                        var startOfMonth = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
+                        var endOfMonth = startOfMonth.AddMonths(1).AddDays(-1);
+                        query = query.Where(a => a.Start >= startOfMonth && a.Start <= endOfMonth);
+                        break;
+
+                    case "By Date Range":
+                        query = query.Where(a => a.Start >= dtStart.Value && a.Start <= dtEnd.Value.AddDays(1).AddSeconds(-1));
+                        break;
+                }
+
+                // Order and execute query
+                _appointments = query.OrderBy(a => a.Start).ToList();
+
+                // Convert times to user's local time
+                foreach (var appt in _appointments)
+                {
+                    appt.Start = TimeZoneHelper.ToUserTime(appt.Start);
+                    appt.End = TimeZoneHelper.ToUserTime(appt.End);
+                }
+
+                // Clear existing rows
+                dgvAppointments.Rows.Clear();
+
+                // Populate grid
+                foreach (var appt in _appointments)
+                {
+                    dgvAppointments.Rows.Add(
+                        appt.AppointmentId,
+                        appt.Title,
+                        appt.Customer?.CustomerName ?? "Unknown",
+                        appt.Type,
+                        appt.Start.ToString("MM/dd/yyyy hh:mm tt"),
+                        appt.End.ToString("MM/dd/yyyy hh:mm tt"),
+                        appt.Location
+                    );
+                }
+
+                // Update button states
+                UpdateButtonStates();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading appointments: {ex.Message}", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void FilterChanged(object sender, EventArgs e)
+        {
+            // Enable/disable date range fields
+            dtStart.Enabled = cbTimeFilter.SelectedItem.ToString() == "By Date Range";
+            dtEnd.Enabled = cbTimeFilter.SelectedItem.ToString() == "By Date Range";
+
+            // Reload appointments with current filter
+            LoadAppointments();
+        }
+
+        private void BtnAddAppointment_Click(object sender, EventArgs e)
+        {
+            // Create a new appointment
+            var appointment = new Appointment
+            {
+                Start = DateTime.Today.AddHours(9), // Default to 9 AM
+                End = DateTime.Today.AddHours(10),  // Default to 1 hour duration
+                UserId = LoginForm.LoggedInUser.UserId
+            };
+
+            var editorForm = new AppointmentEditorForm(appointment, true);
+            if (editorForm.ShowDialog() == DialogResult.OK)
+            {
+                // Refresh appointments
+                LoadAppointments();
+            }
+        }
+
+        private void BtnViewDetails_Click(object sender, EventArgs e)
+        {
+            if (_selectedAppointment != null)
+            {
+                var detailsForm = new AppointmentDetailsForm(_selectedAppointment);
+                detailsForm.ShowDialog();
+            }
+        }
+
+        private void BtnEdit_Click(object sender, EventArgs e)
+        {
+            if (_selectedAppointment != null)
+            {
+                var editorForm = new AppointmentEditorForm(_selectedAppointment, false);
+                if (editorForm.ShowDialog() == DialogResult.OK)
+                {
+                    // Refresh appointments
+                    LoadAppointments();
+                }
+            }
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            if (_selectedAppointment != null)
+            {
+                var result = MessageBox.Show(
+                    $"Are you sure you want to delete the appointment '{_selectedAppointment.Title}'?",
+                    "Confirm Delete",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Warning);
+
+                if (result == DialogResult.Yes)
+                {
+                    try
+                    {
+                        // Remove the appointment from the database
+                        var appointmentToRemove = Program.DbContext.Appointments
+                            .Find(_selectedAppointment.AppointmentId);
+
+                        if (appointmentToRemove != null)
+                        {
+                            Program.DbContext.Appointments.Remove(appointmentToRemove);
+                            Program.DbContext.SaveChanges();
+
+                            // Refresh appointments
+                            LoadAppointments();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error deleting appointment: {ex.Message}", "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void DgvAppointments_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvAppointments.SelectedRows.Count > 0)
+            {
+                int selectedIndex = dgvAppointments.SelectedRows[0].Index;
+                if (selectedIndex >= 0 && selectedIndex < _appointments.Count)
+                {
+                    _selectedAppointment = _appointments[selectedIndex];
+                    UpdateButtonStates();
+                }
+            }
+        }
+
+        private void UpdateButtonStates()
+        {
+            bool appointmentSelected = _selectedAppointment != null;
+            btnViewDetails.Enabled = appointmentSelected;
+            btnEdit.Enabled = appointmentSelected;
+            btnDelete.Enabled = appointmentSelected;
+        }
+
+        // Method to refresh data from outside the control
+        public void RefreshData()
+        {
+            LoadAppointments();
+        }
+
+        // InitializeComponent method should be designed in the Designer 
+        // It will set up the UI elements like:
+        // - DataGridView for appointments
+        // - Buttons for actions
+        // - Time filter dropdown
+        // - Date range pickers
+        private void InitializeComponent()
+        {
+            this.dgvAppointments = new System.Windows.Forms.DataGridView();
+            this.btnAddAppointment = new System.Windows.Forms.Button();
+            this.btnViewDetails = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.cbTimeFilter = new System.Windows.Forms.ComboBox();
+            this.dtStart = new System.Windows.Forms.DateTimePicker();
+            this.dtEnd = new System.Windows.Forms.DateTimePicker();
+
+            // Configure DataGridView columns
+            dgvAppointments.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+                new System.Windows.Forms.DataGridViewTextBoxColumn { Name = "Id", HeaderText = "ID", Visible = false },
+                new System.Windows.Forms.DataGridViewTextBoxColumn { Name = "Title", HeaderText = "Title" },
+                new System.Windows.Forms.DataGridViewTextBoxColumn { Name = "Customer", HeaderText = "Customer" },
+                new System.Windows.Forms.DataGridViewTextBoxColumn { Name = "Type", HeaderText = "Type" },
+                new System.Windows.Forms.DataGridViewTextBoxColumn { Name = "Start", HeaderText = "Start" },
+                new System.Windows.Forms.DataGridViewTextBoxColumn { Name = "End", HeaderText = "End" },
+                new System.Windows.Forms.DataGridViewTextBoxColumn { Name = "Location", HeaderText = "Location" }
+            });
+
+            // Additional UI setup would go here
+            // This is a basic template and would need to be fully implemented in the Designer
+        }
+    }
+}
