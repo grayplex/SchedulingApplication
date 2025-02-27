17ed57dc9da21e2183172d1d778cf9379b7fc790
grayplex <admin@grayplex.com>
2025-02-27 17:26:17 -0600

    Enhance Appointment Alert Form UI and resource management
    Updated `AppointmentAlertForm.Designer.cs` to improve layout,
    initialize ListView column headers, and adjust control properties.
    Introduced a new panel for the ListView and modified button
    anchoring and sizes. Added `AppointmentAlertForm.resx` for
    resource management and updated the project file to include
    the new resource as an embedded resource.

--- AppointmentAlertForm.Designer.cs
+++ AppointmentAlertForm.Designer.cs
@@ -31,51 +31,38 @@
             this.lblHeader = new System.Windows.Forms.Label();
             this.lblAppointmentCount = new System.Windows.Forms.Label();
             this.lstAppointments = new System.Windows.Forms.ListView();
-            this.colTime = new System.Windows.Forms.ColumnHeader();
-            this.colTitle = new System.Windows.Forms.ColumnHeader();
-            this.colCustomer = new System.Windows.Forms.ColumnHeader();
+            this.colTime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colTitle = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colCustomer = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.btnViewDetails = new System.Windows.Forms.Button();
             this.btnClose = new System.Windows.Forms.Button();
-            this.pnlHeader = new System.Windows.Forms.Panel();
-            this.pnlButtons = new System.Windows.Forms.Panel();
-
-            // 
-            // pnlHeader
-            // 
-            this.pnlHeader.Controls.Add(this.lblHeader);
-            this.pnlHeader.Controls.Add(this.lblAppointmentCount);
-            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnlHeader.Location = new System.Drawing.Point(0, 0);
-            this.pnlHeader.Name = "pnlHeader";
-            this.pnlHeader.Size = new System.Drawing.Size(484, 80);
-            this.pnlHeader.TabIndex = 0;
-
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
             // 
             // lblHeader
             // 
             this.lblHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(87)))), ((int)(((byte)(34)))));
             this.lblHeader.Dock = System.Windows.Forms.DockStyle.Top;
-            this.lblHeader.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeader.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
             this.lblHeader.ForeColor = System.Drawing.Color.White;
             this.lblHeader.Location = new System.Drawing.Point(0, 0);
             this.lblHeader.Name = "lblHeader";
             this.lblHeader.Padding = new System.Windows.Forms.Padding(10);
-            this.lblHeader.Size = new System.Drawing.Size(484, 40);
-            this.lblHeader.TabIndex = 0;
+            this.lblHeader.Size = new System.Drawing.Size(528, 52);
+            this.lblHeader.TabIndex = 4;
             this.lblHeader.Text = "Upcoming Appointment Alert";
             this.lblHeader.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-
             // 
             // lblAppointmentCount
             // 
-            this.lblAppointmentCount.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.lblAppointmentCount.Location = new System.Drawing.Point(0, 40);
+            this.lblAppointmentCount.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblAppointmentCount.Location = new System.Drawing.Point(0, 52);
             this.lblAppointmentCount.Name = "lblAppointmentCount";
             this.lblAppointmentCount.Padding = new System.Windows.Forms.Padding(10);
-            this.lblAppointmentCount.Size = new System.Drawing.Size(484, 40);
-            this.lblAppointmentCount.TabIndex = 1;
+            this.lblAppointmentCount.Size = new System.Drawing.Size(528, 39);
+            this.lblAppointmentCount.TabIndex = 3;
             this.lblAppointmentCount.Text = "Upcoming Appointments: 0";
-
             // 
             // lstAppointments
             // 
@@ -87,82 +74,74 @@
             this.lstAppointments.FullRowSelect = true;
             this.lstAppointments.GridLines = true;
             this.lstAppointments.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
-            this.lstAppointments.Location = new System.Drawing.Point(0, 80);
+            this.lstAppointments.HideSelection = false;
+            this.lstAppointments.Location = new System.Drawing.Point(0, 0);
             this.lstAppointments.Name = "lstAppointments";
-            this.lstAppointments.Size = new System.Drawing.Size(484, 251);
-            this.lstAppointments.TabIndex = 1;
+            this.lstAppointments.Size = new System.Drawing.Size(504, 265);
+            this.lstAppointments.TabIndex = 2;
             this.lstAppointments.UseCompatibleStateImageBehavior = false;
             this.lstAppointments.View = System.Windows.Forms.View.Details;
-
             // 
             // colTime
             // 
             this.colTime.Text = "Time";
             this.colTime.Width = 100;
-
             // 
             // colTitle
             // 
             this.colTitle.Text = "Title";
             this.colTitle.Width = 200;
-
             // 
             // colCustomer
             // 
             this.colCustomer.Text = "Customer";
-            this.colCustomer.Width = 180;
-
-            // 
-            // pnlButtons
-            // 
-            this.pnlButtons.Controls.Add(this.btnClose);
-            this.pnlButtons.Controls.Add(this.btnViewDetails);
-            this.pnlButtons.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.pnlButtons.Location = new System.Drawing.Point(0, 331);
-            this.pnlButtons.Name = "pnlButtons";
-            this.pnlButtons.Size = new System.Drawing.Size(484, 50);
-            this.pnlButtons.TabIndex = 2;
-
+            this.colCustomer.Width = 200;
             // 
             // btnViewDetails
             // 
-            this.btnViewDetails.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnViewDetails.Location = new System.Drawing.Point(296, 10);
+            this.btnViewDetails.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnViewDetails.Location = new System.Drawing.Point(360, 365);
             this.btnViewDetails.Name = "btnViewDetails";
-            this.btnViewDetails.Size = new System.Drawing.Size(100, 30);
-            this.btnViewDetails.TabIndex = 0;
+            this.btnViewDetails.Size = new System.Drawing.Size(75, 23);
+            this.btnViewDetails.TabIndex = 1;
             this.btnViewDetails.Text = "View Details";
-            this.btnViewDetails.UseVisualStyleBackColor = true;
-
             // 
             // btnClose
             // 
-            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnClose.Location = new System.Drawing.Point(402, 10);
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.Location = new System.Drawing.Point(441, 365);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(80, 30);
-            this.btnClose.TabIndex = 1;
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 0;
             this.btnClose.Text = "Close";
-            this.btnClose.UseVisualStyleBackColor = true;
-
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lstAppointments);
+            this.panel1.Location = new System.Drawing.Point(12, 94);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(504, 265);
+            this.panel1.TabIndex = 5;
             // 
             // AppointmentAlertForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 381);
-            this.Controls.Add(this.lstAppointments);
-            this.Controls.Add(this.pnlButtons);
-            this.Controls.Add(this.pnlHeader);
+            this.ClientSize = new System.Drawing.Size(528, 400);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnViewDetails);
+            this.Controls.Add(this.lblAppointmentCount);
+            this.Controls.Add(this.lblHeader);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "AppointmentAlertForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Appointment Alert";
-            this.pnlHeader.ResumeLayout(false);
-            this.pnlButtons.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
+
         }
 
         #endregion
@@ -177,5 +156,6 @@
         private System.Windows.Forms.Button btnClose;
         private System.Windows.Forms.Panel pnlHeader;
         private System.Windows.Forms.Panel pnlButtons;
+        private System.Windows.Forms.Panel panel1;
     }
 }
