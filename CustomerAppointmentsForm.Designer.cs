3c81a014cb1f9c212fab3f66ea7e2435cc4a1fc2
grayplex <admin@grayplex.com>
2025-02-27 17:25:32 -0600

    Add DataGridView columns and resource management
    Updated `CustomerAppointmentsForm.Designer.cs` to include new DataGridView columns for appointment details (Id, Title, Consultant, Type, Start, End, Location). Adjusted layout and properties of panels and controls for better UI structure. Added `CustomerAppointmentsForm.resx` for resource management and updated `SchedulingApplication.csproj` to include this new resource file as an embedded resource.

--- CustomerAppointmentsForm.Designer.cs
+++ CustomerAppointmentsForm.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            // Panels
             this.pnlHeader = new System.Windows.Forms.Panel();
             this.lblCustomerName = new System.Windows.Forms.Label();
             this.pnlFilter = new System.Windows.Forms.Panel();
@@ -37,209 +36,270 @@
             this.btnAddAppointment = new System.Windows.Forms.Button();
             this.pnlGrid = new System.Windows.Forms.Panel();
             this.dgvAppointments = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Consultant = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Start = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.End = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Location = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.pnlButtons = new System.Windows.Forms.Panel();
             this.btnViewDetails = new System.Windows.Forms.Button();
             this.btnEdit = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
             this.btnClose = new System.Windows.Forms.Button();
-
+            this.pnlHeader.SuspendLayout();
+            this.pnlFilter.SuspendLayout();
+            this.pnlGrid.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAppointments)).BeginInit();
+            this.pnlButtons.SuspendLayout();
+            this.SuspendLayout();
             // 
             // pnlHeader
             // 
             this.pnlHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(212)))));
+            this.pnlHeader.Controls.Add(this.lblCustomerName);
             this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnlHeader.Height = 60;
-
+            this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(900, 60);
+            this.pnlHeader.TabIndex = 0;
             // 
             // lblCustomerName
             // 
+            this.lblCustomerName.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lblCustomerName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold);
             this.lblCustomerName.ForeColor = System.Drawing.Color.White;
-            this.lblCustomerName.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lblCustomerName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblCustomerName.Location = new System.Drawing.Point(0, 0);
+            this.lblCustomerName.Name = "lblCustomerName";
             this.lblCustomerName.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
-
+            this.lblCustomerName.Size = new System.Drawing.Size(900, 60);
+            this.lblCustomerName.TabIndex = 0;
+            this.lblCustomerName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // pnlFilter
             // 
-            this.pnlFilter.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnlFilter.Height = 60;
             this.pnlFilter.BackColor = System.Drawing.Color.White;
             this.pnlFilter.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-
+            this.pnlFilter.Controls.Add(this.lblFilter);
+            this.pnlFilter.Controls.Add(this.cbFilter);
+            this.pnlFilter.Controls.Add(this.btnAddAppointment);
+            this.pnlFilter.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlFilter.Location = new System.Drawing.Point(0, 60);
+            this.pnlFilter.Name = "pnlFilter";
+            this.pnlFilter.Size = new System.Drawing.Size(900, 80);
+            this.pnlFilter.TabIndex = 1;
             // 
             // lblFilter
             // 
-            this.lblFilter.Text = "Filter:";
             this.lblFilter.Location = new System.Drawing.Point(10, 20);
-            this.lblFilter.Width = 50;
-
+            this.lblFilter.Name = "lblFilter";
+            this.lblFilter.Size = new System.Drawing.Size(50, 23);
+            this.lblFilter.TabIndex = 0;
+            this.lblFilter.Text = "Filter:";
             // 
             // cbFilter
             // 
-            this.cbFilter.Location = new System.Drawing.Point(70, 17);
-            this.cbFilter.Width = 200;
             this.cbFilter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-
+            this.cbFilter.Location = new System.Drawing.Point(70, 17);
+            this.cbFilter.Name = "cbFilter";
+            this.cbFilter.Size = new System.Drawing.Size(200, 21);
+            this.cbFilter.TabIndex = 1;
             // 
             // btnAddAppointment
             // 
-            this.btnAddAppointment.Text = "Add Appointment";
-            this.btnAddAppointment.Location = new System.Drawing.Point(700, 17);
-            this.btnAddAppointment.Width = 120;
             this.btnAddAppointment.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(212)))));
             this.btnAddAppointment.ForeColor = System.Drawing.Color.White;
-
+            this.btnAddAppointment.Location = new System.Drawing.Point(700, 17);
+            this.btnAddAppointment.Name = "btnAddAppointment";
+            this.btnAddAppointment.Size = new System.Drawing.Size(120, 23);
+            this.btnAddAppointment.TabIndex = 2;
+            this.btnAddAppointment.Text = "Add Appointment";
+            this.btnAddAppointment.UseVisualStyleBackColor = false;
             // 
             // pnlGrid
             // 
+            this.pnlGrid.Controls.Add(this.dgvAppointments);
             this.pnlGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlGrid.Location = new System.Drawing.Point(0, 140);
+            this.pnlGrid.Name = "pnlGrid";
             this.pnlGrid.Padding = new System.Windows.Forms.Padding(10);
-
+            this.pnlGrid.Size = new System.Drawing.Size(900, 400);
+            this.pnlGrid.TabIndex = 2;
             // 
             // dgvAppointments
             // 
-            this.dgvAppointments.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dgvAppointments.AllowUserToAddRows = false;
             this.dgvAppointments.AllowUserToDeleteRows = false;
-            this.dgvAppointments.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvAppointments.ReadOnly = true;
-
-            // Configure DataGridView Columns
+            this.dgvAppointments.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dgvAppointments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvAppointments.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-                new System.Windows.Forms.DataGridViewTextBoxColumn {
-                    Name = "Id",
-                    HeaderText = "ID",
-                    Visible = false
-                },
-                new System.Windows.Forms.DataGridViewTextBoxColumn {
-                    Name = "Title",
-                    HeaderText = "Title",
-                    Width = 180
-                },
-                new System.Windows.Forms.DataGridViewTextBoxColumn {
-                    Name = "Consultant",
-                    HeaderText = "Consultant",
-                    Width = 120
-                },
-                new System.Windows.Forms.DataGridViewTextBoxColumn {
-                    Name = "Type",
-                    HeaderText = "Type",
-                    Width = 120
-                },
-                new System.Windows.Forms.DataGridViewTextBoxColumn {
-                    Name = "Start",
-                    HeaderText = "Start",
-                    Width = 170
-                },
-                new System.Windows.Forms.DataGridViewTextBoxColumn {
-                    Name = "End",
-                    HeaderText = "End",
-                    Width = 170
-                },
-                new System.Windows.Forms.DataGridViewTextBoxColumn {
-                    Name = "Location",
-                    HeaderText = "Location",
-                    Width = 150
-                }
-            });
-
+            this.Id,
+            this.Title,
+            this.Consultant,
+            this.Type,
+            this.Start,
+            this.End,
+            this.Location});
+            this.dgvAppointments.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvAppointments.Location = new System.Drawing.Point(10, 10);
+            this.dgvAppointments.MultiSelect = false;
+            this.dgvAppointments.Name = "dgvAppointments";
+            this.dgvAppointments.ReadOnly = true;
+            this.dgvAppointments.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvAppointments.Size = new System.Drawing.Size(880, 380);
+            this.dgvAppointments.TabIndex = 0;
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "ID";
+            this.Id.Name = "Id";
+            this.Id.ReadOnly = true;
+            this.Id.Visible = false;
+            // 
+            // Title
+            // 
+            this.Title.HeaderText = "Title";
+            this.Title.Name = "Title";
+            this.Title.ReadOnly = true;
+            this.Title.Width = 180;
+            // 
+            // Consultant
+            // 
+            this.Consultant.HeaderText = "Consultant";
+            this.Consultant.Name = "Consultant";
+            this.Consultant.ReadOnly = true;
+            this.Consultant.Width = 120;
+            // 
+            // Type
+            // 
+            this.Type.HeaderText = "Type";
+            this.Type.Name = "Type";
+            this.Type.ReadOnly = true;
+            this.Type.Width = 120;
+            // 
+            // Start
+            // 
+            this.Start.HeaderText = "Start";
+            this.Start.Name = "Start";
+            this.Start.ReadOnly = true;
+            this.Start.Width = 170;
+            // 
+            // End
+            // 
+            this.End.HeaderText = "End";
+            this.End.Name = "End";
+            this.End.ReadOnly = true;
+            this.End.Width = 170;
+            // 
+            // Location
+            // 
+            this.Location.HeaderText = "Location";
+            this.Location.Name = "Location";
+            this.Location.ReadOnly = true;
+            this.Location.Width = 150;
             // 
             // pnlButtons
             // 
-            this.pnlButtons.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.pnlButtons.Height = 60;
             this.pnlButtons.BackColor = System.Drawing.SystemColors.Control;
-
+            this.pnlButtons.Controls.Add(this.btnViewDetails);
+            this.pnlButtons.Controls.Add(this.btnEdit);
+            this.pnlButtons.Controls.Add(this.btnDelete);
+            this.pnlButtons.Controls.Add(this.btnClose);
+            this.pnlButtons.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlButtons.Location = new System.Drawing.Point(0, 540);
+            this.pnlButtons.Name = "pnlButtons";
+            this.pnlButtons.Size = new System.Drawing.Size(900, 60);
+            this.pnlButtons.TabIndex = 3;
             // 
             // btnViewDetails
             // 
-            this.btnViewDetails.Text = "View Details";
             this.btnViewDetails.Enabled = false;
             this.btnViewDetails.Location = new System.Drawing.Point(300, 15);
-            this.btnViewDetails.Width = 120;
-
+            this.btnViewDetails.Name = "btnViewDetails";
+            this.btnViewDetails.Size = new System.Drawing.Size(120, 23);
+            this.btnViewDetails.TabIndex = 0;
+            this.btnViewDetails.Text = "View Details";
             // 
             // btnEdit
             // 
-            this.btnEdit.Text = "Edit";
             this.btnEdit.Enabled = false;
             this.btnEdit.Location = new System.Drawing.Point(430, 15);
-            this.btnEdit.Width = 120;
-
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(120, 23);
+            this.btnEdit.TabIndex = 1;
+            this.btnEdit.Text = "Edit";
             // 
             // btnDelete
             // 
-            this.btnDelete.Text = "Delete";
             this.btnDelete.Enabled = false;
             this.btnDelete.Location = new System.Drawing.Point(560, 15);
-            this.btnDelete.Width = 120;
-
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(120, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
             // 
             // btnClose
             // 
-            this.btnClose.Text = "Close";
             this.btnClose.Location = new System.Drawing.Point(690, 15);
-            this.btnClose.Width = 120;
-
-            // Add controls to panels
-            this.pnlHeader.Controls.Add(this.lblCustomerName);
-            this.pnlFilter.Controls.AddRange(new System.Windows.Forms.Control[] {
-                this.lblFilter,
-                this.cbFilter,
-                this.btnAddAppointment
-            });
-            this.pnlGrid.Controls.Add(this.dgvAppointments);
-            this.pnlButtons.Controls.AddRange(new System.Windows.Forms.Control[] {
-                this.btnViewDetails,
-                this.btnEdit,
-                this.btnDelete,
-                this.btnClose
-            });
-
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(120, 23);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
             // 
             // CustomerAppointmentsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(900, 600);
-            this.Controls.AddRange(new System.Windows.Forms.Control[] {
-                this.pnlGrid,
-                this.pnlButtons,
-                this.pnlFilter,
-                this.pnlHeader
-            });
+            this.Controls.Add(this.pnlGrid);
+            this.Controls.Add(this.pnlFilter);
+            this.Controls.Add(this.pnlHeader);
+            this.Controls.Add(this.pnlButtons);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "CustomerAppointmentsForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Customer Appointments";
-
+            this.pnlHeader.ResumeLayout(false);
+            this.pnlFilter.ResumeLayout(false);
+            this.pnlGrid.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAppointments)).EndInit();
+            this.pnlButtons.ResumeLayout(false);
             this.ResumeLayout(false);
+
         }
+
 
         #endregion
 
-        // Header Panel
         private System.Windows.Forms.Panel pnlHeader;
         private System.Windows.Forms.Label lblCustomerName;
-
-        // Filter Panel
         private System.Windows.Forms.Panel pnlFilter;
         private System.Windows.Forms.Label lblFilter;
         private System.Windows.Forms.ComboBox cbFilter;
         private System.Windows.Forms.Button btnAddAppointment;
-
-        // Grid Panel
         private System.Windows.Forms.Panel pnlGrid;
         private System.Windows.Forms.DataGridView dgvAppointments;
-
-        // Buttons Panel
+        private System.Windows.Forms.DataGridViewTextBoxColumn colId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTitle;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colConsultant;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colType;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colStart;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colEnd;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colLocation;
         private System.Windows.Forms.Panel pnlButtons;
         private System.Windows.Forms.Button btnViewDetails;
         private System.Windows.Forms.Button btnEdit;
         private System.Windows.Forms.Button btnDelete;
         private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Title;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Consultant;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Type;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Start;
+        private System.Windows.Forms.DataGridViewTextBoxColumn End;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Location;
     }
 }
